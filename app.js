48c312ec0f1048472f97e824cbaf5763fbb9dc4d
colewendling <colewendling@utexas.edu>
2021-02-19 03:10:28 -0600

    prepare for heroku push

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const passport = require('passport');
+const path = require('path');
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
@@ -27,6 +28,13 @@ app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 
 
