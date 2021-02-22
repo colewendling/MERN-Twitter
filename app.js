9161f6f17bf69504170558c81954ad704d2f650f
colewendling <colewendling@utexas.edu>
2021-02-22 01:37:40 -0600

    MongoDB + Express Router setup

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-const mongoose = require('mongoose');
 const express = require("express");
-const port = process.env.PORT || 5000;
-const db = require('./config/keys').mongoURI;
 const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
